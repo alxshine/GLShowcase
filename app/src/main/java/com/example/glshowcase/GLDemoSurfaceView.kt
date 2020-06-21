8300b4797008831902f73c336401ca7fa35f0f9c
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-06-21 12:27:15 +0200

    Initial Commit, black screen

---
+++ app/src/main/java/com/example/glshowcase/GLDemoSurfaceView.kt
@@ -0,0 +1,14 @@
+package com.example.glshowcase
+
+import android.content.Context
+import android.opengl.GLSurfaceView
+
+class GLDemoSurfaceView(context: Context) : GLSurfaceView(context) {
+    private val renderer: GLDemoRenderer
+
+    init {
+        setEGLContextClientVersion(2)
+        renderer = GLDemoRenderer()
+        setRenderer(renderer)
+    }
+}
---
+++ app/src/main/java/com/example/glshowcase/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.glshowcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val glView = GLDemoSurfaceView(this)
+        setContentView(glView)
+    }
+}
