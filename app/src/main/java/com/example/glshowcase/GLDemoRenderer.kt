9924c91e7082df8501974d138364eb15a357470f
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-06-21 13:06:31 +0200

    Add rendering of triangle

--- app/src/main/java/com/example/glshowcase/GLDemoRenderer.kt
+++ app/src/main/java/com/example/glshowcase/GLDemoRenderer.kt
@@ -6,8 +6,12 @@ import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 class GLDemoRenderer : GLSurfaceView.Renderer {
+    private lateinit var mTriangle: Triangle
+
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         GLES20.glClearColor(0f, 0f, 0f, 1f)
+
+        mTriangle = Triangle()
     }
 
     override fun onSurfaceChanged(unused: GL10?, width: Int, height: Int) {
@@ -16,5 +20,18 @@ class GLDemoRenderer : GLSurfaceView.Renderer {
 
     override fun onDrawFrame(p0: GL10?) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        mTriangle.draw()
+    }
+}
+
+fun loadShader(type: Int, shaderCode: String): Int {
+
+    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+    return GLES20.glCreateShader(type).also { shader ->
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode)
+        GLES20.glCompileShader(shader)
     }
 }
